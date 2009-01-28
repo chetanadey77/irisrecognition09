ea8b4f921640dab9ed41e846176dd05991e0866c
sebsmith42 <sebsmith42@4ee7598a-e738-11dd-bd7b-b12b6590754f>
2009-01-28 16:49:41 +0000

    basic db interface

--- src/iris/database/databaseWrapper.java
+++ src/iris/database/databaseWrapper.java
@@ -1,5 +1,53 @@
 package iris.database;
 
+import java.io.*;
+import java.sql.*;
+
+
 public class databaseWrapper {
 
-}
+	    public static void main (String[] args) {
+
+	        System.out.println( "Testing Postgres access from Java for Iris project\n" );
+
+	        try {
+	            Class.forName("org.postgresql.Driver");
+	        } catch (ClassNotFoundException e) {
+	            System.err.println( "Driver not found: " + e + "\n" + e.getMessage() );
+	        }
+	    
+	        try {
+	            Connection conn = DriverManager.getConnection ("jdbc:postgresql://db.doc.ic.ac.uk/",
+	                                                           "ad108","CV6sDl7jxq");
+	            //This will be substituted for our group id and password once CSG have set up our gr\
+	oup database                                                                                     
+
+	            Statement stmt = conn.createStatement();
+
+	            ResultSet rs;
+
+
+
+	            stmt.executeUpdate("INSERT into iris VALUES(2,'Sebastian');");
+
+	            rs = stmt.executeQuery("SELECT * FROM iris");
+	               while ( rs.next() ) {
+	                int col1 = rs.getInt("a");
+	                String col2 = rs.getString("b");
+	               System.out.println(col1+","+col2);
+	            }
+
+
+	               conn.close();
+	           } catch (Exception e) {
+	               System.err.println("Exception: " + e + "\n" + e.getMessage() );
+	           }
+
+	       }
+
+	   }
+
+
+
+
+
