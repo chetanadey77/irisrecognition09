006cf4b1bb8877016574bf21a2f9c58b60b036aa
en108@doc.ic.ac.uk <en108@doc.ic.ac.uk@4ee7598a-e738-11dd-bd7b-b12b6590754f>
2009-02-09 21:49:12 +0000

    Bitcode now displays, but it looks very similar each time

--- trunk/src/iris/gui/MainGUI.java
+++ trunk/src/iris/gui/MainGUI.java
@@ -1,5 +1,8 @@
 package iris.gui;
 
+import iris.imageToBitcode.BitcodeGenerator;
+import iris.bitcodeMatcher.BitCode;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.EventQueue;
@@ -21,6 +24,8 @@ import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+
+import unittest.ImageToBitcode.ImageSaverLoader;
 
 public class MainGUI {
 
@@ -49,7 +54,9 @@ class MainFrame extends JFrame
 	static JLabel imageEyeTwo = new JLabel();
 	static JLabel imageIrisOne = new JLabel();
 	static JLabel imageIrisTwo = new JLabel();
-
+	static JLabel imageBitCodeOne = new JLabel();
+	static JLabel imageBitCodeTwo = new JLabel();
+	
     static JButton getimageOne = new JButton("Click to load image");
     static JButton getimageTwo = new JButton("Click to load image");
     static JButton editimageOne = new JButton("Edit image");
@@ -63,11 +70,15 @@ class MainFrame extends JFrame
     static ImageIcon iconEyeTwo = new ImageIcon();
     static ImageIcon iconIrisOne = new ImageIcon();
     static ImageIcon iconIrisTwo = new ImageIcon();
+    static ImageIcon iconBitCodeOne = new ImageIcon();
+    static ImageIcon iconBitCodeTwo = new ImageIcon();
     
     static BufferedImage biEyeOne;
     static BufferedImage biEyeTwo;
     static BufferedImage biIrisOne;
     static BufferedImage biIrisTwo;
+    static BufferedImage biBitCodeOne;
+    static BufferedImage biBitCodeTwo;
     
     static EditDialog editdialogOne = null;
     static EditDialog editdialogTwo = null;
@@ -77,7 +88,7 @@ class MainFrame extends JFrame
     static EyeData eyeTwo;
     
     static final int FRAME_WIDTH = 900;
-    static final int FRAME_HEIGHT = 750;
+    static final int FRAME_HEIGHT = 850;
     
     public MainFrame()
         
@@ -107,6 +118,9 @@ class MainFrame extends JFrame
                 biEyeTwo = new BufferedImage(320,280,BufferedImage.TYPE_BYTE_GRAY);
                 biIrisOne = new BufferedImage(320,280,BufferedImage.TYPE_BYTE_GRAY);
                 biIrisTwo = new BufferedImage(320,280,BufferedImage.TYPE_BYTE_GRAY);
+                biBitCodeOne = new BufferedImage(768,24,BufferedImage.TYPE_BYTE_GRAY);
+                biBitCodeTwo = new BufferedImage(768,24,BufferedImage.TYPE_BYTE_GRAY);
+                
                 //set images to alternate squares of grey and white
                 int square_size=10;
                 int grey = 0xe0e0e0;
@@ -121,14 +135,34 @@ class MainFrame extends JFrame
                 			biIrisOne.setRGB(x,y,colour);
                 			biIrisTwo.setRGB(x,y,colour);
                 		}
+                
+                //Do the same for the bitcode images
+                square_size=8;
+                for(int x =0;x<768;x++)
+                	for(int y=0;y<24;y++) 
+                		{
+                			colour=((x/square_size +y/square_size)%2==1)?grey:white;
+                			biBitCodeOne.setRGB(x,y,colour);
+                			biBitCodeTwo.setRGB(x,y,colour);
+                		}
+                
+                
                 iconEyeOne.setImage(biEyeOne);
                 iconEyeTwo.setImage(biEyeTwo);
                 iconIrisOne.setImage(biIrisOne);
                 iconIrisTwo.setImage(biIrisTwo);
+                iconBitCodeOne.setImage(biBitCodeOne);
+                iconBitCodeTwo.setImage(biBitCodeTwo);
+                
+                
                 imageEyeOne.setIcon(iconEyeOne);
                 imageEyeTwo.setIcon(iconEyeTwo);
                 imageIrisOne.setIcon(iconIrisOne);
                 imageIrisTwo.setIcon(iconIrisTwo);
+                imageBitCodeOne.setIcon(iconBitCodeOne);
+                imageBitCodeTwo.setIcon(iconBitCodeTwo);
+                
+                
 //              create a panel for the first eye
                 JPanel paneleyeOne = new JPanel();
                 paneleyeOne.add(panelbuttonOne);
@@ -141,9 +175,11 @@ class MainFrame extends JFrame
                 paneleyeTwo.add(imageIrisTwo);
                 
                 getContentPane().add(paneleyeOne);
+                getContentPane().add(imageBitCodeOne);
                 compare.setEnabled(false);
                 getContentPane().add(compare);
                 getContentPane().add(paneleyeTwo);
+                getContentPane().add(imageBitCodeTwo);
                 //Handle the get Image buttons
                 getimageOne.addActionListener(new ActionListener() {
                     public void actionPerformed(ActionEvent arg0) {
@@ -172,7 +208,8 @@ class MainFrame extends JFrame
                             
                             editdialogOne.setVisible(true);
                         	//System.out.println("returned from edit");
-                        	copyIris(biEyeOne,biIrisOne,editdialogOne.getEyeData());
+                            eyeOne = editdialogOne.getEyeData();
+                        	copyIris(biEyeOne,biIrisOne,eyeOne);
                         	 iconIrisOne.setImage(biIrisOne);
                              imageIrisOne.setIcon(iconIrisOne);
                              imageIrisOne.repaint();
@@ -190,12 +227,79 @@ class MainFrame extends JFrame
                         
                         editdialogTwo.setVisible(true);
                     	//System.out.println("returned from edit");
-                    	copyIris(biEyeTwo,biIrisTwo,editdialogTwo.getEyeData());
+                        eyeTwo = editdialogTwo.getEyeData();
+                    	copyIris(biEyeTwo,biIrisTwo,eyeTwo);
                     	 iconIrisTwo.setImage(biIrisTwo);
                          imageIrisTwo.setIcon(iconIrisTwo);
                          imageIrisTwo.repaint();
                          bitcodeimageTwo.setEnabled(true);
                         }
+        });
+                bitcodeimageOne.addActionListener(new ActionListener() {
+                	public void actionPerformed(ActionEvent arg0) {
+                		long startTime=System.currentTimeMillis(); //calculate runtime
+                		//ImageSaverLoader isl = new ImageSaverLoader(); 
+                		//BufferedImage eyeball = isl.loadImage("eye.bmp");
+                		BitcodeGenerator b = new BitcodeGenerator();
+                		BitCode bc =  b.getBitcode(biEyeOne,eyeOne.inner.x,eyeOne.inner.y,eyeOne.inner.radius,eyeOne.outer.x,eyeOne.outer.y,eyeOne.outer.radius);
+                		int[] intarr = bc.getBitCode();
+                		biBitCodeOne = bc.getBitCodeImage(768,24,8);
+                		iconBitCodeOne.setImage(biBitCodeOne);
+                        imageBitCodeOne.setIcon(iconBitCodeOne);
+                        imageBitCodeOne.repaint();
+                		
+                		for (int i=0; i < intarr.length; i++)
+                		{
+                			System.out.println(i + ".: " + Integer.toBinaryString(intarr[i]) + " :: " + intarr[i] );
+                		}
+                		System.out.println("Running time: " + (float)(System.currentTimeMillis() - startTime)/1000 + " seconds");
+                		int bit=0;
+                		for (int i=0; i < intarr.length; i++)
+                		{
+                			System.out.print(i + ".: ");
+                			for(int j=0;j<32;j++){
+                				if (bc.getBit(bit))
+                					System.out.print(1);
+                				else 
+                					System.out.print(0);
+                				bit++;
+                			}
+                			System.out.println();
+                		}
+                	
+                        }
+            });
+                bitcodeimageTwo.addActionListener(new ActionListener() {
+            	public void actionPerformed(ActionEvent arg0) {
+            		long startTime=System.currentTimeMillis(); //calculate runtime
+            		BitcodeGenerator b = new BitcodeGenerator();
+            		BitCode bc =  b.getBitcode(biEyeTwo,eyeTwo.inner.x,eyeTwo.inner.y,eyeTwo.inner.radius,eyeTwo.outer.x,eyeTwo.outer.y,eyeTwo.outer.radius);
+            		int[] intarr = bc.getBitCode();
+            		biBitCodeTwo = bc.getBitCodeImage(768,24,8);
+            		iconBitCodeTwo.setImage(biBitCodeTwo);
+                    imageBitCodeTwo.setIcon(iconBitCodeTwo);
+                    imageBitCodeTwo.repaint();
+            		
+            		for (int i=0; i < intarr.length; i++)
+            		{
+            			System.out.println(i + ".: " + Integer.toBinaryString(intarr[i]) + " :: " + intarr[i] );
+            		}
+            		System.out.println("Running time: " + (float)(System.currentTimeMillis() - startTime)/1000 + " seconds");
+            		int bit=0;
+            		for (int i=0; i < intarr.length; i++)
+            		{
+            			System.out.print(i + ".: ");
+            			for(int j=0;j<32;j++){
+            				if (bc.getBit(bit))
+            					System.out.print(1);
+            				else 
+            					System.out.print(0);
+            				bit++;
+            			}
+            			System.out.println();
+            		}
+            	
+                    }
         });
         }
     private static void copyIris(BufferedImage bifrom, BufferedImage bito, EyeData ed )
