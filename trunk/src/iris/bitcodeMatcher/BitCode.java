b798f501fde083d4eb7833e2a8bf53e50bb6d6cc
en108@doc.ic.ac.uk <en108@doc.ic.ac.uk@4ee7598a-e738-11dd-bd7b-b12b6590754f>
2009-02-14 15:05:38 +0000

    Changed the hamming distance method to return a double (between 0 and 1) and it now shifts over some of its length

--- trunk/src/iris/bitcodeMatcher/BitCode.java
+++ trunk/src/iris/bitcodeMatcher/BitCode.java
@@ -86,10 +86,43 @@ public class BitCode extends BitSet {
 		return bi;
 	}
 	
-	public int hammingDistance(BitCode b)
-	{
-		BitSet bs = (BitSet) b.clone();
-		bs.xor(this);
-		return bs.cardinality();
+	public static double hammingDistance(BitCode ba, BitCode bb)
+	{	final int rotation = 10; //number of degrees (from -rotation to +rotation)
+		//rotation =0 should compare with no rotation
+		//need to work out where to get the mask from
+		//it is likely to be either part of the bit code for the eye
+		//or we can pass another bitset object  for each of the eyes
+		
+		//bit set should return a double between 0.0 and 1.0
+		// calculated by cardinality(a xor b and maska and maskb)/cardinality (maska and maskb)
+		
+		//I think that we should do the shifting in this function 
+		
+		//In the paper from 2004 below 0.32 showed a very high chance of non independence
+		
+		BitSet bsa = (BitSet) ba.clone();
+		//BitSet bsma = (BitSet) maska.clone();
+		double min_hamming=1.0;
+		double hc;
+		//doesn't seem to be a bitset shift function
+		for (int i = - ba.size()*rotation/360;i<= ba.size()*rotation/360; i++)
+		{
+			bsa.clear();
+			//bsma.clear();
+			for(int j =0;j<ba.size();j++)
+			{
+				if (ba.get((i+ba.size()+j)%ba.size())) bsa.set(j,true);
+				//need the same for mask a
+			}
+
+			bsa.xor(bb);
+			System.out.println(bsa.size() +"  "+bsa.cardinality());
+			hc = (double) (bsa.cardinality())/(double)(bsa.size()) ;
+			if (min_hamming>hc) min_hamming=hc;
+			//need to include the masks in the above
+		}
+		
+		bsa.xor(bb);
+		return min_hamming;
 	}
 }
