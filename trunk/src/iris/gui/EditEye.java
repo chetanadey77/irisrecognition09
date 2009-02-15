e72128f4d85ec95de6b7067d48a1e6cbc0f108fd
en108@doc.ic.ac.uk <en108@doc.ic.ac.uk@4ee7598a-e738-11dd-bd7b-b12b6590754f>
2009-02-15 12:32:39 +0000

    Added routine to find pupil and iris outer boundary, works sometimes.

--- trunk/src/iris/gui/EditEye.java
+++ trunk/src/iris/gui/EditEye.java
@@ -44,8 +44,8 @@ class EditDialog extends JDialog
          int orad = 97;
          SpinnerNumberModel modelix = new SpinnerNumberModel(ix,0,319,1);
          SpinnerNumberModel modeliy = new SpinnerNumberModel(iy,0,279,-1);
-         SpinnerNumberModel modelox = new SpinnerNumberModel(ix,0,319,1);
-         SpinnerNumberModel modeloy = new SpinnerNumberModel(iy,0,279,-1);
+         SpinnerNumberModel modelox = new SpinnerNumberModel(ox,0,319,1);
+         SpinnerNumberModel modeloy = new SpinnerNumberModel(oy,0,279,-1);
          SpinnerNumberModel modelir = new SpinnerNumberModel(irad,0,159,1);
          SpinnerNumberModel modelor = new SpinnerNumberModel(orad,0,139,1);
          JSpinner innerx = new JSpinner(modelix);
@@ -81,6 +81,10 @@ class EditDialog extends JDialog
                 setSize(500,600);
                 setLayout(new FlowLayout());
                 
+                EyeData ed = find_iris(bi);
+                ox=ed.outer.x;
+                oy=ed.outer.y;
+                orad=ed.outer.radius;
                 inner.add(new JLabel("Inner Circle"));
                 inner.add(innerx);
                 inner.add(innery);
@@ -186,7 +190,7 @@ class EditDialog extends JDialog
         	EyeData ed = new EyeData(ix,iy,irad,ox,oy,orad);
         	return ed;
         }
-        public BufferedImage gaussian_blur(BufferedImage bi,int pixels)
+        public static BufferedImage gaussian_blur(BufferedImage bi,int pixels)
         {
         	float[] matrix = new float[(pixels*2+1)*(pixels*2+1)] ;
         	double sigma = (double) pixels / 3.0;
@@ -209,18 +213,104 @@ class EditDialog extends JDialog
         	return nbi;
         	
         }
-        public CircleType find_circle(BufferedImage bi, int pixel_blur, Bounds bounds)
+        public static CircleType find_circle(BufferedImage bi, int pixel_blur, char octant,Bounds bounds)
         {
+        	BufferedImage bigb = gaussian_blur(bi,3);
+        	CircleType c = new CircleType();
+        	double max_diff = 0.0;
+        	double diff = 0.0;
+        	int xo=0,yo=0,ro=0;
         	for (int radius= bounds.rmin;radius<=bounds.rmax;radius++)
         		for (int x =bounds.xmin + radius;x<=bounds.xmax-radius;x++)
         			for (int y =bounds.ymin + radius;y<=bounds.ymax-radius;y++)
         			{
-        				if ()
+        				diff = Math.abs(loop_integral(bigb,x,y,radius,octant)- loop_integral(bigb,x,y,radius-1,octant));
+        				if (diff>max_diff) 
+        				{
+        					max_diff = diff;
+        					xo=x; yo=y; ro=radius;
+        				}
         			}
-                			
-        	CircleType c = new CircleType();
+            c.x = xo;
+            c.y = yo;
+            c.radius = ro;
         	return c;	
         }
+        public static double loop_integral(BufferedImage bi, int cenx, int ceny, int r, char octant)
+        {
+        	int total=0;
+        	int count =0;
+        	//Algorthim modified from Bresenham 1977
+        	int x=0;
+        	int y=r;
+        	double d =5.0/ 4.0 - (double) r;
+        	//normally will do 1/8th of the circle and then test 8
+        	//versions but first and last only get tested 4 times
+        	if ((octant & 129)>0) {total+=bi.getRGB(cenx+x,ceny+y);count++;}
+        	if ((octant & 6)>0) {total+=bi.getRGB(cenx+y,ceny+x);count++;}
+        	if ((octant & 24)>0) {total+=bi.getRGB(cenx+x,ceny-y);count++;}
+        	if ((octant & 96)>0) {total+=bi.getRGB(cenx-y,ceny+x);count++;}
+        	while(y>x)
+        	{
+        		if (d<0) d+= 2.0*(double)x+3.0;
+        		else
+        		{
+        			d += 2.0*(double)(x-y)+5.0;
+        			y--;
+        		}
+        		x++;
+        		if ((octant & 1)>0) {total+=bi.getRGB(cenx+x,ceny+y);count++;}
+        		if ((octant & 2)>0) {total+=bi.getRGB(cenx+y,ceny+x);count++;}
+        		if ((octant & 4)>0) {total+=bi.getRGB(cenx+y,ceny-x);count++;}
+        		if ((octant & 8)>0) {total+=bi.getRGB(cenx+x,ceny-y);count++;}
+        		if ((octant & 16)>0) {total+=bi.getRGB(cenx-x,ceny+y);count++;}
+        		if ((octant & 32)>0) {total+=bi.getRGB(cenx-y,ceny+x);count++;}
+        		if ((octant & 64)>0) {total+=bi.getRGB(cenx-y,ceny-x);count++;}
+        		if ((octant & 128)>0) {total+=bi.getRGB(cenx-x,ceny-y);count++;}
+        		
+        	}
+        	if(y==x)
+        	{
+        		if ((octant & 3)>0) {total+=bi.getRGB(cenx+x,ceny+x);count++;}
+        		if ((octant & 12)>0) {total+=bi.getRGB(cenx+x,ceny-x);count++;}
+        		if ((octant & 48)>0) {total+=bi.getRGB(cenx-x,ceny-x);count++;}
+        		if ((octant & 192)>0) {total+=bi.getRGB(cenx-x,ceny+x);count++;}
+        	}
+        	
+        	
+        	if (count>0) return (double) (total) /(double)count;
+        	else return 0;
+        }
+        public static EyeData find_iris(BufferedImage bi)
+        {
+        	EyeData ed = new EyeData();
+        	Bounds b = new Bounds();
+            int blur=20;
+            b.rmin = 30;// specification is iris must be at least 70 pixels in diameter
+            b.rmin=80;//just for testing
+            b.xmin = blur;
+            b.ymin = blur;
+            b.xmax = bi.getWidth() -blur;
+            b.ymax = bi.getHeight() -blur;
+            b.rmax = (bi.getHeight())/2;
+            
+            
+            CircleType c = find_circle(bi,blur,(char)102,b);
+            ed.outer = c;
+            
+            /*blur=2;
+            b.xmin = bl;
+            b.ymin = blur;
+            b.xmax = bi.getWidth() - blur;
+            b.ymax = bi.getHeight() - blur;
+            b.rmax = b.xmax-b.xmin;
+            b.rmin = 30;// specification is iris must be at least 70 pixels in diameter
+            
+            c = find_circle(bi,10,(char)231,b);
+            */
+            return ed;
+        }
+        
 }
 // controls how much of the image will be searched for a circle
 // in particular used to find pupil (which we know is inside iris)
@@ -234,9 +324,6 @@ class Bounds
 	
 	public int rmin;
 	public int rmax;
-	
-	public int thetamin;
-	public int thetamax;
 }
 
        
